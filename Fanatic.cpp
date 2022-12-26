c0852dda0e14276948698df863646233b835fe61
lgb <lawleegb@naver.com>
2022-12-26 09:25:32 +0900

    Commit Add ImageName

--- Fanatic.cpp
+++ Fanatic.cpp
@@ -3,32 +3,32 @@
 
 void Fanatic::Init()
 {
-	m_vimage[eIdle] = IMAGEMANAGER->FindImageVector("Fanaticidle");
+	m_vimage[eIdle] = IMAGEMANAGER->FindImageVector("Fanatic_Idle");
 	m_vimage[eIdle]->Setting(0.3f, true);
 	
-	m_vimage[eAttackIdle] = IMAGEMANAGER->FindImageVector("FanaticattackIdle");
+	m_vimage[eAttackIdle] = IMAGEMANAGER->FindImageVector("Fanatic_AttackIdle");
 	m_vimage[eAttackIdle]->Setting(0.3f, true);
 
-	m_vimage[eAttack] = IMAGEMANAGER->FindImageVector("Fanaticattack");
+	m_vimage[eAttack] = IMAGEMANAGER->FindImageVector("Fanatic_Attack");
 	m_vimage[eAttack]->Setting(0.3f, true);
 
-	m_vimage[eRunAttack] = IMAGEMANAGER->FindImageVector("Fanaticrunattack");
+	m_vimage[eRunAttack] = IMAGEMANAGER->FindImageVector("Fanatic_Runattack");
 	m_vimage[eRunAttack]->Setting(0.3f, true);
 
-	m_vimage[eRun] = IMAGEMANAGER->FindImageVector("Fanaticrun");
+	m_vimage[eRun] = IMAGEMANAGER->FindImageVector("Fanatic_Run");
 	m_vimage[eRun]->Setting(0.3f, true);
 
-	m_vimage[Setcrifice] = IMAGEMANAGER->FindImageVector("Fanaticsetcrifice");
+	m_vimage[Setcrifice] = IMAGEMANAGER->FindImageVector("Fanatic_Setcrifice");
 	m_vimage[Setcrifice]->Setting(0.3f, true);
 		
 
-	m_vimage[SetcrificeLoop] = IMAGEMANAGER->FindImageVector("Fanaticsetcrificeloop");
+	m_vimage[SetcrificeLoop] = IMAGEMANAGER->FindImageVector("Fanatic_Setcrificeloop");
 	m_vimage[SetcrificeLoop]->Setting(0.3f, true);
 	
-	m_vimage[SetcrificeReady] = IMAGEMANAGER->FindImageVector("Fanaticsetcrificeready");
+	m_vimage[SetcrificeReady] = IMAGEMANAGER->FindImageVector("Fanatic_Setcrificeready");
 	m_vimage[SetcrificeReady]->Setting(0.3f, true);
 
-	m_vimage[eHit] = IMAGEMANAGER->FindImageVector("Fanatichit");
+	m_vimage[eHit] = IMAGEMANAGER->FindImageVector("Fanatic_Hit");
 	m_vimage[eHit]->Setting(0.3f, true);
 	
 }
