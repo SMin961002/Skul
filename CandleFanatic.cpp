500b5f7e4e893c621a6d64bf41398da05b337c61
lgb <lawleegb@naver.com>
2023-01-06 11:22:54 +0900

    Commit candleFantic collion

--- CandleFanatic.cpp
+++ CandleFanatic.cpp
@@ -6,9 +6,14 @@
 #include "RigidBodyComponent.h"
 #include "PixelCollisionComponent.h"
 #include "FireKnife.h"
+#include"BlackRock.h"
+#include"Gold.h"
+#include"Effect.h"
 
 void CandleFanatic::Init()
 {
+	m_maxhp = 100.0f;
+	m_currenthp = 100.0f;
 	m_isL = false;
 	m_vimage[eIdle] = IMAGEMANAGER->FindImageVector("Cfanatic_Idle");
 	m_vimage[eIdle]->Setting(0.1f, true);
@@ -33,52 +38,89 @@ void CandleFanatic::Init()
 
 	m_vimage[eSatcrificeLoop] = IMAGEMANAGER->FindImageVector("Cfanatic_SatcrificeLoop");
 	m_vimage[eSatcrificeLoop]->Setting(0.1f, false);
-	m_state = eIdle;
-	m_obj->AddComponent<PixelCollisionComponent>()->setting(SCENEMANAGER->m_tiles, &m_obj->x, &m_obj->y);
+	
+	m_vimage[eHPbarEmpty] = IMAGEMANAGER->AddImageVectorCopy("Hpbar_Empty");
+	m_vimage[eHPbarEmpty]->Setting(0.4f, false);
 
+	m_vimage[eHpbarDown] = IMAGEMANAGER->AddImageVectorCopy("Hpbar_Down");
+	m_vimage[eHpbarDown]->Setting(0.4f, false);
+
+	m_vimage[eHpbarUp] = IMAGEMANAGER->AddImageVectorCopy("Hpbar_Up");
+	m_vimage[eHpbarUp]->Setting(0.4f, false);
+
+	m_state = eIdle;
+	effect = new Appear;
+	effect->Init();
+	effect->SetObject(m_obj);
+	effect->SetEffectStart(m_obj->x, m_obj->y, false, 1.5);
+	m_collision = m_obj->AddComponent<CollisionComponent>();
+	m_obj->AddCollisionComponent(m_collision);
+	m_obj->AddComponent<PixelCollisionComponent>()->setting(SCENEMANAGER->m_tiles, &m_obj->x, &m_obj->y);
 	m_obj->AddComponent<RigidBodyComponent>();
 }
 
 void CandleFanatic::Update()
 {
-	if ((abs(m_obj->x - OBJECTMANAGER->m_player->GetplayerX()) < 400) || m_state == eAttackReady || m_state == eAttack)
+	if (m_effect == false)
 	{
-		if (m_vimage[eAttackReady]->GetIsImageEnded() == true)
+		m_obj->GetComponent<RigidBodyComponent>()->SetIsActive(false);
+	}
+	if (effect->GetIsEffectEnded())
+	{
+		m_effect = true;
+	}
+	
+	if (m_effect == true)
+	{
+		m_obj->GetComponent<RigidBodyComponent>()->SetIsActive(true);
+		m_collision->Setting(40, m_obj->x + 17, m_obj->y - 20, "Attack");
+		if ((abs(m_obj->x - OBJECTMANAGER->m_player->GetplayerX()) < 400) || m_state == eAttackReady || m_state == eAttack)
 		{
-			if (m_state == eAttackReady)
+			if (m_vimage[eAttackReady]->GetIsImageEnded() == true)
 			{
-				OBJECTMANAGER->AddObject("Knife", m_obj->x, m_obj->y - 100, eObject)->AddComponent<FireKnife>();
+				if (m_state == eAttackReady)
+				{
+					OBJECTMANAGER->AddObject("Knife", m_obj->x, m_obj->y - 100, eObject)->AddComponent<FireKnife>();
+				}
+				m_state = eAttack;
+				if (m_vimage[m_state]->GetIsImageEnded() == true)
+				{
+					m_vimage[eAttackReady]->Reset();
+					m_vimage[eAttack]->Reset();
+					m_vimage[eIdle]->Reset();
+					m_state = eIdle;
+				}
 			}
-			m_state = eAttack;
-			if (m_vimage[m_state]->GetIsImageEnded() == true)
+			else
 			{
-				m_vimage[eAttackReady]->Reset();
-				m_vimage[eAttack]->Reset();
-				m_vimage[eIdle]->Reset();
-				m_state = eIdle;
+				m_state = eAttackReady;
+				m_isL = OBJECTMANAGER->m_player->GetplayerX() < m_obj->x;
 			}
+		}
+		else if (abs(m_obj->x - OBJECTMANAGER->m_player->GetplayerX()) < 600)
+		{
+			m_state = eRun;
+			m_obj->x += DELTA_TIME * (OBJECTMANAGER->m_player->GetplayerX() > m_obj->x ? 30 : -30);
+			m_isL = OBJECTMANAGER->m_player->GetplayerX() < m_obj->x;
 		}
 		else
 		{
-			m_state = eAttackReady;
-			m_isL = OBJECTMANAGER->m_player->GetplayerX() < m_obj->x;
+			m_state = eIdle;
 		}
+		m_hpbar = (1 / m_maxhp);
+		m_hiteffecttimer += DELTA_TIME;
 	}
-	else if (abs(m_obj->x - OBJECTMANAGER->m_player->GetplayerX()) < 600)
-	{
-		m_state = eRun;
-		m_obj->x += DELTA_TIME * (OBJECTMANAGER->m_player->GetplayerX() > m_obj->x ? 30 : -30);
-		m_isL = OBJECTMANAGER->m_player->GetplayerX() < m_obj->x;
-	}
-	else
-	{
-		m_state = eIdle;
-	}
+	cout << m_currenthp << endl;
+	
 }
 
 void CandleFanatic::Render()
 {
-	m_vimage[m_state]->CenterRender(m_obj->x, m_obj->y-60, 2, 2, 0, m_isL);
+	effect->Render();
+	if (m_effect == true)
+	{
+		m_vimage[m_state]->CenterRender(m_obj->x, m_obj->y - 60, 2, 2, 0, m_isL);
+	}
 }
 
 void CandleFanatic::Release()
@@ -87,4 +129,38 @@ void CandleFanatic::Release()
 
 void CandleFanatic::OnCollision(string collisionName, Object* other)
 {
+	if (collisionName == m_collision->GetName())
+	{
+		if (other->GetName() == "player")
+		{
+			//other->GetComponent<Player>()-플레이어 상태쉉
+			m_attack = true;
+			m_move = false;
+		}
+	}
+
+}
+
+void CandleFanatic::HitEnemy(float dmg)
+{
+	if (m_hiteffecttimer >= 0.7f)
+	{
+		m_vimage[eHit]->Reset();
+		m_obj->x += m_obj->x > OBJECTMANAGER->m_player->GetplayerX() ? DELTA_TIME * 500 : -DELTA_TIME * 500;
+		m_obj->y -= 10;
+		m_obj->GetComponent<RigidBodyComponent>()->SetIsActive(false);
+		m_currenthp -= dmg;
+		
+		if (OBJECTMANAGER->m_player->GetplayerX() <= m_obj->x)
+		{
+			EFFECTMANAGER->AddEffect<SkulAttack>(m_obj->x - 5, m_obj->y - 10, 0, 2);
+		}
+		else
+		{
+			EFFECTMANAGER->AddEffect<SkulAttack>(m_obj->x - 5, m_obj->y - 10, 1, 2);
+		}
+		m_hit = true;
+		m_hiteffecttimer = 0;
+		
+	}
 }
--- CandleFanatic.h
+++ CandleFanatic.h
@@ -15,18 +15,29 @@ private:
 		eSatcrifice,
 		eSatcrificeReady,
 		eSatcrificeLoop,
+		eHpbarUp,
+		eHpbarDown,
+		eHPbarEmpty,
 		eEnd
 	};
 
 	vImage* m_vimage[eEnd];
 	bool m_attack;
+	bool m_effect;
+	bool m_hit;
+	bool m_move;
+	
+
+	CollisionComponent* m_collision;
+
+	float m_hiteffecttimer;
 
 public:
 	virtual void Init() override;
 	virtual void Update() override;
 	virtual void Render() override;
 	virtual void Release() override;
+	virtual void HitEnemy(float dmg) override;
 	virtual void OnCollision(string collisionName, Object* other) override;
-
 };
 
