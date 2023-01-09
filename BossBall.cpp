9d9b7c12c2e82bfa8ca53e16d53341a758ec50a1
DESKTOP-2PRCJ99\taewo <taewooo0513@gmail.com>
2023-01-09 23:56:00 +0900

    merge to main or Add BossPatter

--- BossBall.cpp
+++ BossBall.cpp
@@ -13,18 +13,39 @@ void BossBall::Init()
 {
 	startX = m_obj->x;
 	startY = m_obj->y;
-
+	ldist = false;
 	m_state = eStart;
 	img[eStart] = IMAGEMANAGER->AddImageVectorCopy("Phase2_Ball_CreateBall_Start");
+	img[eStart]->Setting(0.1, false);
 	img[eIdle] = IMAGEMANAGER->AddImageVectorCopy("Phase2_Ball_CreateBall_Idle");
+	img[eIdle]->Setting(0.07, true);
 	//img[eDis] = IMAGEMANAGER->AddImageVectorCopy("");
 }
 
 void BossBall::Update()
 {
-	if (dist < 250)
+	if (ldist == false)
 	{
-		dist += 0.3;
+		if (dist < 200)
+		{
+			dist += 1;
+		}
+		else
+		{
+			if (dist > 200)
+			{
+				dist -= 1;
+			}
+			rot += 0.01;
+		}
+	}
+	else
+	{
+		if (dist < 500)
+		{
+			dist += 1;
+		}
+		rot += 0.01;
 	}
 
 	m_obj->x = startX + cos(rot) * dist;
@@ -33,6 +54,10 @@ void BossBall::Update()
 
 void BossBall::Render()
 {
+	if (img[eStart]->GetIsImageEnded() == true)
+	{
+		m_state = eIdle;
+	}
 	img[m_state]->CenterRender(m_obj->x, m_obj->y, 1.5, 1.5, 0);
 }
 
